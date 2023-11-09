77a255e63b14b2decc9605352b429d23c490c002
Omkar Shigvan <omishigvan99@gmail.com>
2023-11-09 23:09:22 +0530

    Added P2P socket handling
    - handling of P2P socket

--- main.js
+++ main.js
@@ -7,11 +7,35 @@ const dotenv = require("dotenv");
 const app = express();
 const httpServer = http.createServer(app);
 const io = new socketIO.Server(httpServer, {
-    // options
+    cors: {
+        origin: "*",
+    },
 });
 
 // configuring environment variables
 dotenv.config();
+
+io.on("connection", (socket) => {
+    console.log("Client:", socket.id);
+
+    socket.on("message", (message) => {
+        console.log(message);
+    });
+
+    socket.on("join-room", (room) => {
+        socket.join(room);
+        console.log("SocketID:", socket.id, " Room: ", room);
+    });
+
+    socket.on("move", (move, room) => {
+        console.log(move, room);
+        socket.to(room).emit("move", move);
+    });
+
+    socket.on("disconnection", (socket) => {
+        console.log(`${socket.id} got disconnected`);
+    });
+});
 
 // server listening
 httpServer.listen(process.env.PORT || 8686, () => {
